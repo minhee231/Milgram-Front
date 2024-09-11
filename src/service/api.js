038c50862a265989e35ba2de0b85a5490a50f6d2
minhee231 <goominhee123@gmail.com>
2024-09-11 19:37:42 +0900

    파비콘 변경

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://api.milgram.x-coll.com',
     headers: {
         'Content-Type': 'application/json',
     }
